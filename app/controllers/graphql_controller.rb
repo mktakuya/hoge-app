63208edc76ff85b51359fd6ea5ddc004d623dcc8
Takuya Mukohira <takuya.mk96@gmail.com>
2021-05-14 15:29:23 +0900

    Protect graphql endpoints by Secured concern

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -5,6 +5,8 @@ class GraphqlController < ApplicationController
   # but you'll have to authenticate your user separately
   # protect_from_forgery with: :null_session
 
+  include Secured
+
   def execute
     variables = prepare_variables(params[:variables])
     query = params[:query]
